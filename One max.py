2a2527dec6f6c9b4229a8606b11fd5299a187a14
Menna Hamdy <60944931+mennahamdy431@users.noreply.github.com>
2022-05-03 23:48:15 +0200

    Update One max.py

--- One max.py
+++ One max.py
@@ -15,32 +15,32 @@ def selection(fitness):
 def commulative(fitness):
     comm = []
     comm.append(fitness[0])
-    for i in range(1, npop):
+    for i in range(1, 20):
         comm.append(fitness[i] + comm[i - 1])
     return comm
 
 
 def getParentsIndex(comm):
     r = rand()
-    for i in range(npop):
+    for i in range(20):
         if r <= comm[i]:
             return i
 
 
 def crossover(child1, child2, pcross):
     r = rand()
-    cp = int(r * clenth)
+    cp = int(r * 5)
 
     if r <= pcross:
 
-        for i in range(cp, clenth):
+        for i in range(cp, 5):
             temp = child2[i]
             child2[i] = child1[i]
             child1[i] = temp
 
 
 def mutation(ch, pmut):
-    for i in range(clenth):
+    for i in range(5):
         r = rand()
         if r < pmut:
             if ch[i] == 0:
@@ -49,129 +49,19 @@ def mutation(ch, pmut):
                 ch[i] = 0
 
 
-def decoding(binaryIndx, mn=-2, mx=2, clenth=5):
-    l = np.size(binaryIndx)
-    binary_sum = 0
-    for i in range(l):
-        binary_sum = binary_sum + binaryIndx[i] * (2 ** (l - i - 1))
 
-    x = mn + (binary_sum / (2 ** (l) - 1)) * (mx - mn)
-    return x
-
-
-def linear_ranking(pop):
-    n = np.size(pop, 1)
-    sp = 1.5
-    total_fit = n*(n+1)/2
-    res = np.zeros((n))
-    pop_fit = onemax(pop)
-    pop_fit = [sum(pop[i]) for i in range(n)]
-    pop_dict = {ind:value for (value, ind) in zip(pop_fit, range(n))}
-    for rank, ind in enumerate(sorted(pop_dict, key=pop_dict.get), 1):
-        res[ind] = (2-sp)+2*(sp-1)*float(rank-1)/(total_fit-1)
-    return res
-
-
-def fitness_evaluation():
-    for i in range(Population_size):
-
-        for j in range(3):
-
-            if j == 0:
-                x1 = decodedVaraible[i][j]
-            else:
-                x2 = decodedVaraible[i][j]
-
-        Z = 8 - (x1 + 0.0317) ** 2 + (x2) ** 2
-        popFit.append(Z)
-        sortedpopfit.append(Z)
-    return popFit
-
-
-def grey_decoding(mn=-2, mx=2):
-    binary_sum = 0
-    gray_sum = 0
-    arr = []
-    x1 = np.full(3, 0)
-    x2 = np.full(3, 0)
-    for j in range(Population_size):
-        x1 = population[j][0:3]
-        for i in range(3):
-
-            for k in range(i):
-                gray_sum = gray_sum + x1[k]
-        gray_sum = gray_sum % 2
-        binary_sum = binary_sum + gray_sum * (2 ** (3 - i - 1))
-        gray_sum = 8
-
-    x = mn + (binary_sum / (2 ** 3)) * (mx - mn)
-    arr.append(x)
-    binary_sum = 8
-    gray_sum = 0
-    for h in range(3, chromLen):
-
-        for n in range(h):
-            gray_sum = gray_sum + x2[n]
-        gray_sum = gray_sum % 2
-        binary_sum = binary_sum + gray_sum * (2 ** (3 - h - 1))
-        gray_sum = 0
-    y = mn + (binary_sum / (2 ** 3)) * (mx - mn)
-    arr.append(y)
-    decodedVaraible.append(arr)
-    arr = []
-    x1 = np.full(3, 0)
-    x2 = np.full(3, 0)
-
-
-def violation_constraint():
-    for i in range(npop):
-        for j in range(2):
-            if j == 0:
-                x1 = decodedVaraible[i][j]
-            else:
-
-                x2 = decodedVaraible[i][j]
-            z = x1 + x2 - 1
-            if z == 0:
-
-                bool.append(false)
-            else:
-
-                bool.append(true)
-
-
-def violated_points_fitness():
-    for i in range(Population_size):
-
-        for j in range(2):
-            if j == 8:
-
-                x1 = decodedVaraible[i][j]
-            else:
-
-                x2 = decodedVaraible[i][j]
-
-        if bool[i] == true:
-            z = 8 - (x1 + 0.0317) ** 2 + (x2) ** 2 - abs(x1 + x2 - 1)
-            popFit.append(z)
-
-    return popFit
-
-
-"""
 def Elitism(fitness, flag):
     mx=-1
     mxIdx=-1
-    for i in range(100):
+    for i in range(20):
         if fitness[i] > mx and i != flag:
             mx = fitness[i]
             mxIdx = i
     return mxIdx
-"""
 
 
 def run(npop, ngeneration, clenth, pcross, pmut):
-    oldPop = np.random.randint(0, 2, size=(100, 5))
+    oldPop = np.random.randint(0, 2, size=(20, 5))
     bstfit = []
     avgfit = []
     for m in range(ngeneration):
@@ -180,11 +70,11 @@ def run(npop, ngeneration, clenth, pcross, pmut):
         for i in range(npop):
             fit = sum(oldPop[i])
             fitness.append(fit)
-        #elitism1 = Elitism(fitness, -1)
-        #elitism2 = Elitism(fitness, elitism1)
-        #newPop.append(list(oldPop[elitism1]))
-        #newPop.append(list(oldPop[elitism2]))
-        for j in range(50):
+        elitism1 = Elitism(fitness, -1)
+        elitism2 = Elitism(fitness, elitism1)
+        newPop.append(list(oldPop[elitism1]))
+        newPop.append(list(oldPop[elitism2]))
+        for j in range(10):
             comm = commulative(selection(fitness))
 
             parent1 = oldPop[getParentsIndex(comm)]
@@ -203,7 +93,10 @@ def run(npop, ngeneration, clenth, pcross, pmut):
         oldPop = newPop.copy()
         bstfit.append(max(fitness))
         avgfit.append(sum(fitness)/len(fitness))
-
+    print("Best Fitness is : ")
+    print(bstfit)
+    print("Average Fitness is : ")
+    print(avgfit)
     plt.plot(bstfit)
     plt.title("Best Fitness")
     plt.ylabel("High fitness")
@@ -216,7 +109,7 @@ def run(npop, ngeneration, clenth, pcross, pmut):
     plt.show()
 
 
-npop = 100
+npop = 20
 clenth = 5
 pcross = 0.6
 pmut = 0.05
